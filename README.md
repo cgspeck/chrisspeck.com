415a61e04687184989860887585f734c29982793
Chris Speck <cgspeck@gmail.com>
2020-07-23 21:50:25 +1000

    Preparing site for production / answering Lighthouse audits

---
+++ README.md
@@ -0,0 +1,20 @@
+# My Gatsby Powered Site
+
+## Installing pre-requisites
+
+Setup nodenv, install the required node version, install then run `yarn`.
+
+## Quick Reference
+
+Run development server with hot-reload:
+
+```
+yarn run gatsby develop
+```
+
+Do a production build & serve:
+
+```
+yarn run gatsby build
+yarn run gatsby serve
+```
--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,20 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Chris Speck's web site`,
+        short_name: `Chris Speck's web site`,
+        start_url: `/`,
+        background_color: `#f5f5f5`,
+        theme_color: `#f5f5f5`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
